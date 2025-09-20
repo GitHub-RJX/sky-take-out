c2caf1e63c58e74cc0de1ebf3b6bed7c1c1a6b82
ranjiaxing <2321471499@qq.com>
2025-09-20 22:10:29 +0800

    【店铺管理】模块完结

---
+++ sky-common/src/main/java/com/sky/constant/RedisConstant.java
@@ -0,0 +1,10 @@
+package com.sky.constant;
+
+/**
+ * Redis键值常量
+ */
+public class RedisConstant {
+
+    // 店铺营业状态
+    public static final String SHOP_STATUS = "SHOP_STATUS";
+}
--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.RedisConstant;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -13,28 +14,30 @@ import org.springframework.web.bind.annotation.*;
 @Api(tags = "店铺相关接口")
 @Slf4j
 public class ShopController {
-
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
     /**
      * 修改店铺营业状态
-     * @param status
-     * @return
+     *
+     * @param status 店铺营业状态
      */
     @PutMapping("/{status}")
     @ApiOperation("设置店铺营业状态")
-    public Result setStatus(@PathVariable Integer status) {
-        log.info("设置店铺营业状态:{}", status == 1 ? "营业" : "打烊");
-        redisTemplate.opsForValue().set("SHOP_STATUS", status);
+    public Result<Void> setStatus(@PathVariable Integer status) {
+//        log.info("设置店铺营业状态:{}", status == StatusConstant.ENABLE ? "营业" : "打烊");
+        redisTemplate.opsForValue().set(RedisConstant.SHOP_STATUS, status);
         return Result.success();
     }
 
+    /**
+     * 获取店铺营业状态
+     */
     @GetMapping("/status")
     @ApiOperation("获取店铺营业状态")
     public Result<Integer> getStatus() {
-        Integer status = (Integer) redisTemplate.opsForValue().get("SHOP_STATUS");
-        log.info("获取店铺营业状态:{}", status == 1 ? "营业" : "打烊");
+        Integer status = (Integer) redisTemplate.opsForValue().get(RedisConstant.SHOP_STATUS);
+//        log.info("获取店铺营业状态:{}", status == StatusConstant.ENABLE ? "营业" : "打烊");
         return Result.success(status);
     }
 }
